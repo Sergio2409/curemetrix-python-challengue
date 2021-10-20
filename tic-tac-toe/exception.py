a9ec38882d4d770023b93b4e5f8d479dd6e3178c
Sergio A Valdes Rabelo <sergiovades2409@gmail.com>
2021-10-20 01:45:52 -0600

    Include known exceptions

---
+++ tic-tac-toe/exception.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# -----------------------------------------------
+# Copyright (c) ░s░e░r░g░i░o░v░a░l░d░e░s░2░4░0░9░
+# Mail: sergiovaldes2409@gmail.com
+#
+# All rights reserved.
+#
+#
+from __future__ import (division as _py3_division,
+                        print_function as _py3_print,
+                        absolute_import as _py3_abs_import)
+
+
+class InvalidMove(Exception):
+    pass
+
+
+class NotAvailableMoves(Exception):
+    pass
